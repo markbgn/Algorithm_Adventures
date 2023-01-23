fde8c68f7567c03e56405c4a44844e6f6d2b0be6
Mark Bogan <boganmark63@gmail.com>
2023-01-23 01:05:42 +0100

    Add project files.

---
+++ Algorithm_Adventures/Container/SortingMethods.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Adventures.Container
+{
+    public class SortingMethods
+    {
+        /// <summary>
+        /// Insert sort algorithm.
+        /// </summary>
+        /// <param name="numbers"></param>
+        public void insertSort(int[] numbers)
+        {
+            int number;
+            int i;
+
+            for (int j = 1; j < numbers.Length; j++)
+            {
+                number = numbers[j];
+                i = j - 1;
+                while (i > -1 && numbers[i] < number)
+                {
+                    numbers[i + 1] = numbers[i];
+                    i--;
+                }
+                numbers[i + 1] = number;
+            }
+        }
+        /// <summary>
+        /// Merge sort algorithm.
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <param name="p"></param>
+        /// <param name="q"></param>
+        /// <param name="r"></param>
+        public void mergeSort(int[] numbers, int p, int q, int r)
+        {
+            int n1 = q - p + 1;
+            int n2 = r - q;
+
+            List<int> L = new List<int>();
+            List<int> R = new List<int>();
+
+            for (int n = 0; n < n1; n++)
+            {
+                L.Add(numbers[p + n]);
+            }
+
+            for (int m = 0; m < n2; m++)
+            {
+                R.Add(numbers[q + m + 1]);
+            }
+
+            int i = 0;
+            int j = 0;
+            for (int k = p; k < r; k++)
+            {
+                if (L[i] <= R[j])
+                {
+                    numbers[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    numbers[k] = R[j];
+                    j++;
+                }
+            }
+        }
+    }
+}
---
+++ Algorithm_Adventures/Program.cs
@@ -0,0 +1,42 @@
+﻿using Algorithm_Adventures.Container;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Adventures
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            SortingMethods sorting = new SortingMethods();
+            // Array to sort
+            int[] numbers = { 1, 7, 9, 10, 13, 16, 4, 5, 11, 14, 22 };
+
+            // Creating arrays without reference
+            int[] numbersInsert = numbers.ToArray();
+            int[] numbersMerge = numbers.ToArray();
+
+            // Insertion sort
+            sorting.insertSort(numbersInsert);
+
+            Console.WriteLine("Numbers sorted with insertion sort:");
+            Console.WriteLine(string.Join(" ", numbersInsert));
+
+            // Merge sort
+            sorting.mergeSort(numbersMerge, 0, 5, numbersMerge.Length-1);
+
+            Console.WriteLine("Numbers sorted with merge sort:");
+            Console.WriteLine(string.Join(" ", numbersMerge));
+
+            while (Console.ReadKey().Key != ConsoleKey.Enter)
+            {
+                //run loop until Enter is pressed
+            }
+        }
+    }
+}
